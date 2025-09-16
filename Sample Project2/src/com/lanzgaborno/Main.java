faf50ddd0e946d818669b38638a10c6a4d49813e
ambulanz <lanzgaborno@gmail.com>
2025-09-16 11:24:27 +0800

    Added Sample Project 2

---
+++ Sample Project2/src/com/lanzgaborno/Main.java
@@ -0,0 +1,19 @@
+package com.lanzgaborno;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// equivalent to writing "cout << "Hello World"
+    // System.out.println("Hello World");
+
+    // gets user input
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("What is your name");
+    String name = scanner.nextLine();
+
+    System.out.println("Hello " + name + "! Welcome to GDSTRUC!");
+    }
+}
